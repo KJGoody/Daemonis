3eb2f0dc86e12bbf552df21fdd0066518f8b2d21
KJGoody <lionkj98@naver.com>
2022-04-02 12:30:05 +0900

    Revert "Revert "64""
    This reverts commit 0b1c2d911f2f70c552e87460adfc903979a5649a.

--- Assets/3. Scripts/Items/Item.cs
+++ Assets/3. Scripts/Items/Item.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 
+public enum Quality { Normal, Advanced, Rare, Epic , Legendary , Relic }
+
 public abstract class Item : ScriptableObject, IMoveable , IDescribable
 {
     [SerializeField]
     private Sprite icon;
     [SerializeField]
-    private string title;
-
-    [SerializeField]
     private int stackSize;
+    [SerializeField]
+    private string title;
+    [SerializeField]
+    private Quality quality;
 
     private SlotScript slot;
 
@@ -52,7 +55,26 @@ public abstract class Item : ScriptableObject, IMoveable , IDescribable
     }
     public string GetDescription()
     {
-        return title;
+        string color = string.Empty;
+
+        switch (quality)
+        {
+            case Quality.Normal:
+                color = "#d6d6d6";
+                break;
+            case Quality.Advanced:
+                color = "#00ff00ff";
+                break;
+            case Quality.Rare:
+                color = "#0000ffff";
+                break;
+            case Quality.Epic:
+                color = "#800080ff";
+                break;
+        }
+
+        return string.Format("<color={0}>{1}</color>", color, title);
     }
+
 
 }
