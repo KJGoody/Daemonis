cd1affdb13f76c144580cf48600ac836d22993dd
KJGoody <lionkj98@naver.com>
2022-04-02 12:35:17 +0900

    Revert "Merge branch '경준' of https://github.com/KJGoody/Daemonis into 경준"
    This reverts commit 0ee64619091af0301d8b98b63049bc98c13f6eb8, reversing
    changes made to 5d72a00d87bc1fb80007ed558ae41924eb2caa69.

---
+++ Assets/3. Scripts/Buttons/BagButton.cs
@@ -0,0 +1,42 @@
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.EventSystems;
+
+//public class BagButton : MonoBehaviour, IPointerClickHandler
+//{
+//    private Bag bag;
+
+//    [SerializeField]
+//    private Sprite full, empty;
+//    public void OnPointerClick(PointerEventData eventData)
+//    {
+//        if (bag != null)
+//        {
+//            bag.MyBagScript.OpenClose();
+//        }
+//    }
+
+//    public Bag MyBag
+//    {
+//        get
+//        {
+//            return bag;
+//        }
+
+//        set
+//        {
+//            // 백 버튼에 가방이 등록되어있는지 아닌지 체크
+//            if (value != null)
+//            {
+//                GetComponent<Image>().sprite = full;
+//            }
+
+//            else
+//            {
+//                GetComponent<Image>().sprite = empty;
+//            }
+
+//            bag = value;
+//        }
+//    }
+//}
---
+++ Assets/3. Scripts/Items/HealthPotion.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 상단 메뉴 버튼 만들기
+[CreateAssetMenu(fileName = "HealthPotion", menuName = "Items/Potion", order = 2)]
+public class HealthPotion : Item, IUseable
+{
+    // 포션아이템의 회복량
+    [SerializeField]
+    private int health;
+
+    // 아이템 사용시
+    public void Use()
+    {
+        // 체력이 최대체력보다 낮으면
+        if (Player.MyInstance.MyHealth.MyCurrentValue < Player.MyInstance.MyHealth.MyMaxValue)
+        {
+            // 사용하는 아이템을 없애고
+            Remove();
+
+            // 체력을 회복한다.
+            Player.MyInstance.MyHealth.MyCurrentValue += health;
+        }
+    }
+    public override string GetDescription()
+    {
+        return base.GetDescription() + string.Format("\n<color=#00ff00ff>Use: 체력 {0} 회복</color>", health);
+    }
+}
