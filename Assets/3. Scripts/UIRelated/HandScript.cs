0b5fd1e780a47750ccf62f7cfebbb5349e84bffa
KJGoody <lionkj98@naver.com>
2022-04-15 21:13:41 +0900

    Revert "Revert ".""
    This reverts commit 81e586737564e493f3d0a53d57d25c2ea32ec9c9.

--- Assets/3. Scripts/UIRelated/HandScript.cs
+++ Assets/3. Scripts/UIRelated/HandScript.cs
@@ -19,6 +19,8 @@ public class HandScript : MonoBehaviour
             return instance;
         }
     }
+    private Spell spell;
+    [Header ("Select Skill")]
     #region 스킬 선택관련 변수
     [SerializeField]
     private Text selectName;
@@ -30,27 +32,42 @@ public class HandScript : MonoBehaviour
     private GameObject blindPanel;
     private bool skillEquipping = false;
     #endregion
-    #region 아이템 선택관련 변수 SI = Select Item
+
     private Item myItem;    // 아이템 정보
+    [Header ("Select Item Tooltip")]
+    #region 아이템 선택관련 변수 SI = Select Item
     [SerializeField]
-    private Image SI_Image;// 선택한 아이템 화면에 보이는 이미지
+    private Image SI_Image; // 선택한 아이템 화면에 보이는 이미지
     [SerializeField]
-    private Text SI_Name; // 아이템 이름
+    private Text SI_Name;   // 아이템 이름
     [SerializeField]
     private Text SI_LimitLvl;// 제한 레벨
     [SerializeField]
     private Text SI_DefaultStat;// 기본효과(기본스탯같은) 설명
     [SerializeField]
+    private Text SI_Descript;// 아이템 배경?설명 (아이템 소개)
+    [SerializeField]
+    private Text SI_Quality;// 아이템 등급
+    [SerializeField]
     private GameObject SI_Panel;// 선택아이템 패널
+    [SerializeField]
+    private GameObject SI_Obj_Option;// 추가옵션 오브젝트
+    [SerializeField]
+    private GameObject SI_Obj_SetOption;// 세트옵션 오브젝트
+    [SerializeField]
+    private GameObject SI_Obj_Blind;// 블라인드 패널 오브젝트
+    [SerializeField]
+    private ContentSizeFitter SI_CSF_Descript;
+    [SerializeField]
+    private ContentSizeFitter SI_CSF_Panel;
     #endregion
 
     // IMoveable은 Spell에서 상속받는다.
     public IMoveable MyMoveable { get; set; }
 
     private Image icon;
-    private Spell spell;
     [SerializeField]
-    private Vector3 offset;
+    private Vector3 offset; // 없어도됨
 
     private void Start()
     {
@@ -85,10 +102,10 @@ public class HandScript : MonoBehaviour
         if (selectImage.sprite != null)
         {
             MyMoveable = spell;
-            BlindControll();
+            SkillBlindControll();
         }
     }
-    public void BlindControll() // 스킬 등록할때 나오는 검은화면
+    public void SkillBlindControll() // 스킬 등록할때 나오는 검은화면
     {
         skillEquipping = !skillEquipping;
         if (!skillEquipping)
@@ -107,19 +124,51 @@ public class HandScript : MonoBehaviour
     public void SelectItem(Item item) // 아이템 선택
     {
         myItem = item;
-        SI_DefaultStat.text = "사용 효과 : "+myItem.MyEffect;
         SI_Image.sprite = myItem.MyIcon;
+        SI_Name.text = myItem.MyName;
+        SI_Quality.text = myItem.MyQuality;
         SI_LimitLvl.text = "제한 레벨 : "+myItem.MyLimitLevel;
-        SI_Name.text = ""+myItem.MyName;
+        SI_DefaultStat.text = "사용 효과 : "+myItem.MyEffect;
+        SI_Descript.text = myItem.MyDescript;
+        switch (myItem.GetKind)
+        {
+            case Kinds.Potion:
+                SI_Obj_Option.SetActive(false);
+                SI_Obj_SetOption.SetActive(false);
+                break;
+            default:
+                break;
+        }
         SI_Panel.SetActive(true);
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)SI_CSF_Descript.transform); // content size filtter 바로 안늘어나는 버그 해결
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)SI_CSF_Panel.transform);
     }
-    public IMoveable Put()
+    public void EquipPotion() // 포션 등록 시작
+    {
+        MyMoveable = myItem;
+        SI_Panel.SetActive(false);
+        SI_Obj_Blind.SetActive(true);
+    }
+    public void ResetEquipPotion() // 포션 등록 취소
+    {
+        MyMoveable = null;
+        SI_Obj_Blind.SetActive(false);
+
+    }
+    public void Close_SI_Panel() // 선택 아이템 패널 닫기
+    {
+        SI_Panel.SetActive(false);
+    }
+    public IMoveable Put() // MyMoveable에 넣어져있는것 슬롯에 넣는 함수
     {
         IMoveable tmp = MyMoveable;
         // 복사한 스킬 아이콘 정보를 null 로 만든다.
         MyMoveable = null;
         // 복사된 아이콘을 투명하게 만든다.
         icon.color = new Color(0, 0, 0, 0);
+
+        if(myItem.GetKind == Kinds.Potion)
+            HandScript.MyInstance.ResetEquipPotion(); // 이거만 포션 전용 코드
         // 복사한 스킬의 아이콘 정보를 전달한다.
         return tmp;
     }
