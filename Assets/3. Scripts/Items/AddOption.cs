b9b936c89a52385c806cc6e0b7ff2a2531fee886
KJGoody <lionkj98@naver.com>
2022-05-31 00:18:57 +0900

    추가옵션, 몬스터 아트작업 시작
    추가옵션 스크립트 생성만 해둠
    몬스터 폴더만들고 모자 만듬

---
+++ Assets/3. Scripts/Items/AddOption.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddOption : MonoBehaviour
+{
+    public int tier;
+    public string option;
+    public float value;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+}
