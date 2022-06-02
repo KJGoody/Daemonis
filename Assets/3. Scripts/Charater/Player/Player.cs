a91218543d62aaabff8d06f7f19ba3756beab862
KJGoody <lionkj98@naver.com>
2022-06-03 04:31:48 +0900

    Update Player.cs

--- Assets/3. Scripts/Charater/Player/Player.cs
+++ Assets/3. Scripts/Charater/Player/Player.cs
@@ -304,10 +304,10 @@ public class Player : Character
         PropertyInfo optionName = stat.GetType().GetProperty(option);
 
         //float a = (float)System.Convert.ToDouble(optionName.GetValue(stat));
-        Debug.Log(optionName.GetValue(stat));
-        Debug.Log(optionName.GetValue(stat).GetType());
 
         float b = (float)System.Convert.ToDouble(optionName.GetValue(stat));
-        optionName.SetValue(stat,  b + value);
+        Debug.Log(b.GetType());
+        Debug.Log(value.GetType());
+        optionName.SetValue(stat,  (int)(b + value));
     }
 }
